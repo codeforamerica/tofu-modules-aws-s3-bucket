db57c96e1381d069ab74ad4f6e935548ca19fc71
James Armes <jarmes@codeforamerica.org>
2026-07-22 13:39:21 -0400

    fix: Remove unused provider constraints.

--- modules/uploads/versions.tf
+++ modules/uploads/versions.tf
@@ -1,15 +1,3 @@
 terraform {
   required_version = ">= 1.10"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 6.28"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.0"
-    }
-  }
 }
